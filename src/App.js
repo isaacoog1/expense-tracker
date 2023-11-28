006ac73f92234f15787fbd32b55f694a8f7084cd
isaacoog1 <oguntuyokoredeshile@gmail.com>
2023-11-28 22:13:20 +0100

    Added all other components

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import Header from "./components/Header";
 import "./App.css";
 import Balance from "./components/Balance";
 import IncomeExpenses from "./components/IncomeExpenses";
+import TransactionList from "./components/TransactionList";
+import AddTransaction from "./components/AddTransaction";
+import { GlobalProvider } from "./context/GlobalState";
 
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
       <div className="container">
         <Balance />
         <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
